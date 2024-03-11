12568014b22087b158c2e37341a5cf0f64183ff3
solotole <solomontoleak47@gmail.com>
2024-03-11 06:07:28 -0700

    changes in soritng algorithms

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,5 +1,24 @@
 #include "sort.h"
 /**
+ * determine - determine what to store in array
+ * count_array
+ * @array: the array to be sorted
+ * @count_array: the count array
+ * size: size of the array to be sorted
+ * @i: index of array
+ * @value: value at index i
+ */
+void determine(int *array, int *count_array, size_t size, size_t i, int value)
+{
+	size_t j;
+
+	for (j = i; j < size; j++)
+	{
+		if (value == array[j])
+			count_array[value] += 1;
+	}
+}
+/**
  * counting_sort - sorting array using counting sort algorithm
  * @array: array to be sorted
  * @size: size of the array
@@ -9,10 +28,10 @@
 void counting_sort(int *array, size_t size)
 {
 	int value, k;
-	size_t i, j, max = array[0];
-	int *count_array = NULL, *new = NULL, count = 0;
+	size_t i, max = array[0];
+	int *count_array = NULL, *new = NULL;
 
-	for(i = 1; i < size; i++)
+	for (i = 1; i < size; i++)
 	{
 		if ((int)max < array[i])
 			max = array[i];
@@ -21,41 +40,25 @@ void counting_sort(int *array, size_t size)
 	new = malloc(sizeof(int) * size);
 	if (count_array == NULL || new == NULL)
 		return;
-	for(i = 0; i < max; i++)
+	for (i = 0; i <= max + 1; i++)
 		count_array[i] = 0;
-	for(i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		value = array[i];
 		if (value == 0 && count_array[value] == 0)
-		{
-			for(j = i + 1; j < size; j++)
-			{
-				if (array[j] == value)
-					count++;
-			}
-			count_array[value] = count + 1;
-			count = 0;
-		}
-		else if(count_array[value] == 0 && value != 0)
-		{
-			for(j = i + 1; j < size; j++)
-			{
-				if (array[j] == value)
-					count++;
-			}
-			count_array[value] = count + 1;
-			count = 0;
-		}
+			determine(array, count_array, size, i, value);
+		else if (count_array[value] == 0 && value != 0)
+			determine(array, count_array, size, i, value);
 	}
-	for(i = 1; i < max + 2; i++)
+	for (i = 1; i < max + 2; i++)
 		count_array[i] += count_array[i - 1];
 	print_array(count_array, max + 1);
-	for(k = size - 1; k >= 0; k--)
+	for (k = size - 1; k >= 0; k--)
 	{
 		new[count_array[array[k]] - 1] = array[k];
 		count_array[array[k]]--;
 	}
-	for(i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 		array[i] = new[i];
 	free(new);
 	free(count_array);
--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -4,27 +4,24 @@
  * selection_sort - sorts a list of number in ascending order
  * @array: an array of numbers
  * @size: size of the array
- * 
 */
-
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j;
+	size_t i, j;
+	int minindex, temp;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        int temp, minindex = i;
-
-
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[minindex])
-                minindex = j;
-        }
-
-        temp = array[i];
-        array[i] = array[minindex];
-        array[minindex] = temp;
-        print_array(array, size);
-    }
+	for (i = 0; i < size - 1; i++)
+	{
+		minindex = i;
+		for (j = i + 1; j < size; j++)
+		{
+			value = array[j];
+			if (value < array[minindex])
+				minindex = j;
+		}
+		temp = array[i];
+		array[i] = array[minindex];
+		array[minindex] = temp;
+		print_array(array, size);
+	}
 }
