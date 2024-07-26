bebacfc8ebd94e27a02cf5ed60da8453a04cc388
Solotole <solomontoleak47@gmail.com>
2024-07-26 07:00:09 +0100

    sort header file

--- sort.h
+++ sort.h
@@ -33,6 +33,7 @@ void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 /*void determine(int *array, int *count_array, size_t size, size_t i, int value);*/
 
 #endif
